2a261a186b53b2f5369ec01a43f4798801864a8a
Student <Student@192.168.152.128>
2018-08-29 09:22:29 -0400

    update Order class

--- src/main/java/com/acme/acmetrade/domain/entities/Order.java
+++ src/main/java/com/acme/acmetrade/domain/entities/Order.java
@@ -20,7 +20,7 @@ public class Order {
 	
 	public Order(UUID traderId, UUID companyId, OrderSide OrderSide, OrderType OrderType, double price, int volume, Date placementTime,
 			OrderStatus OrderStatus) {
-		super();
+		
 		this.traderId = traderId;
 		this.companyId = companyId;
 		this.OrderSide = OrderSide;
@@ -29,6 +29,74 @@ public class Order {
 		this.volume = volume;
 		this.placementTime = placementTime;
 		this.OrderStatus = OrderStatus;
+	}
+
+	public UUID getTraderId() {
+		return traderId;
+	}
+
+	public void setTraderId(UUID traderId) {
+		this.traderId = traderId;
+	}
+
+	public UUID getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(UUID companyId) {
+		this.companyId = companyId;
+	}
+
+	public OrderSide getOrderSide() {
+		return OrderSide;
+	}
+
+	public void setOrderSide(OrderSide orderSide) {
+		OrderSide = orderSide;
+	}
+
+	public OrderType getOrderType() {
+		return OrderType;
+	}
+
+	public void setOrderType(OrderType orderType) {
+		OrderType = orderType;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public Date getPlacementTime() {
+		return placementTime;
+	}
+
+	public void setPlacementTime(Date placementTime) {
+		this.placementTime = placementTime;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		OrderStatus = orderStatus;
+	}
+
+	public UUID getId() {
+		return id;
 	}
 	
 	
