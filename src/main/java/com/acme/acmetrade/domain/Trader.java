27cfa83a220ae84d17bdbdcedb1b1ee206c430a0
Student <Student@192.168.152.128>
2018-08-29 07:09:34 -0400

    Trader_class

---
+++ src/main/java/com/acme/acmetrade/domain/Trader.java
@@ -0,0 +1,82 @@
+package com.acme.acmetrade.domain;
+
+public class Trader {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String email;
+	private String phone;
+	private boolean canTrade;
+	
+	public Trader(int id, String firstName, String lastName, String address, String email, String phone,
+			boolean canTrade) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.email = email;
+		this.phone = phone;
+		this.canTrade = canTrade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public boolean isCanTrade() {
+		return canTrade;
+	}
+
+	public void setCanTrade(boolean canTrade) {
+		this.canTrade = canTrade;
+	}
+	
+	
+	
+}
+	
