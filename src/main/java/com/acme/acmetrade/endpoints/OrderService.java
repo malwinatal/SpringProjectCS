407e588c616076e90031dc78f61e6425fba22649
Student Name <student@email.com>
2018-08-30 12:01:37 -0400

    clean orderService

--- src/main/java/com/acme/acmetrade/endpoints/OrderService.java
+++ src/main/java/com/acme/acmetrade/endpoints/OrderService.java
@@ -1,7 +1,5 @@
 package com.acme.acmetrade.endpoints;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.acme.acmetrade.domain.OrderStatus;
 import com.acme.acmetrade.domain.entities.Order;
 import com.acme.acmetrade.repository.OrderRepository;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,18 +25,13 @@ public class OrderService {
 	@RequestMapping(value="/order", method = RequestMethod.POST)
 	public void placeOrder(@RequestBody Order order) {
 			orderList.add(order);
-			//Order myOrder  = new MarketOrder(Currency.EUR, 100, Side.BUY);
 			myDB.placeOrder(order);
 
 	}
 	
 	@RequestMapping(value="/order", method = RequestMethod.PATCH)
 	public void cancelOrder(@RequestParam("orderID") String orderID) {
-			//orderList.add(order);
-			//Order myOrder  = new MarketOrder(Currency.EUR, 100, Side.BUY);
-			
 			myDB.cancelOrder(UUID.fromString(orderID));
-
 	}
 	
 	/**
@@ -50,13 +41,6 @@ public class OrderService {
 	 */
 	@RequestMapping(value="/order/update", method = RequestMethod.PATCH) //change URI
 	public void updateOrder(@RequestBody Order order) {
-
-		if (order.getOrderStatus()== OrderStatus.FULFILLED || order.getOrderStatus()== OrderStatus.CANCELLED) {
-			System.out.println("Warning: Order cannot be updated as it is already fulfilled or cancelled");
-		} 
-		else {
 			myDB.updateOrder(order);
-		}
-
 	}
 }
