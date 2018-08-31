ce6339f15eb3f29dc05db568f7039afbf8902152
Student Name <student@email.com>
2018-08-31 05:15:13 -0400

    change uri for user story 3

--- src/main/java/com/acme/acmetrade/endpoints/OrderService.java
+++ src/main/java/com/acme/acmetrade/endpoints/OrderService.java
@@ -2,6 +2,7 @@ package com.acme.acmetrade.endpoints;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,12 +35,7 @@ public class OrderService {
 			myDB.cancelOrder(UUID.fromString(orderID));
 	}
 	
-	/**
-	 * updates a trader order
-	 * @param order
-	 * @return
-	 */
-	@RequestMapping(value="/order/update", method = RequestMethod.PATCH) //change URI
+	@RequestMapping(value="/order/updatingWithId", method = RequestMethod.PATCH) //change URI
 	public void updateOrder(@RequestBody Order order) {
 			myDB.updateOrder(order);
 	}
